c8197fa6a46eca5195c4b65feddb0e3d9f6f70ae
Kevin Zhang <kevinzhang@utexas.edu>
2019-02-04 17:19:26 -0600

    first commit

---
+++ tumblr/PhotoCell.swift
@@ -0,0 +1,24 @@
+//
+//  PhotoCell.swift
+//  tumblr
+//
+//  Created by Kevin Zhang on 1/30/19.
+//  Copyright © 2019 Kevin Zhang. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
